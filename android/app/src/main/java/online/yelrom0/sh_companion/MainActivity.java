1969b93997fba8c1c1ec7d8e1f55258c18af534f
yelrom0 <yelrom0@gmail.com>
2024-03-25 11:06:14 +1100

    chore: init

---
+++ android/app/src/main/java/online/yelrom0/sh_companion/MainActivity.java
@@ -0,0 +1,5 @@
+package online.yelrom0.sh_companion;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'online.yelrom0.sh_companion',
+  appName: 'Spacial Home Companion',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

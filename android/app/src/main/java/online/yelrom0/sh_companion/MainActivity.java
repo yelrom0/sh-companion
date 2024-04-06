191d145b19e9de7db24efe38da8960c72e987c8c
yelrom0 <yelrom0@gmail.com>
2024-04-07 00:41:18 +1100

    feat(Android): Add plugin

--- android/app/src/main/java/online/yelrom0/sh_companion/MainActivity.java
+++ android/app/src/main/java/online/yelrom0/sh_companion/MainActivity.java
@@ -2,10 +2,4 @@ package online.yelrom0.sh_companion;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(NotificationPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
